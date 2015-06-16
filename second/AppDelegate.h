01fccd3b888f6fa8f04aa299db48e0fc695ec2df
johny babylon <ztredded@gmail.com>
2015-06-15 23:37:44 -0700

    Initial Commit

---
+++ second/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  second
+//
+//  Created by elliott chavis on 6/15/15.
+//  Copyright (c) 2015 elliott chavis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ second/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  second
+//
+//  Created by elliott chavis on 6/15/15.
+//  Copyright (c) 2015 elliott chavis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
